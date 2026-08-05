7fcfdcac2de822a92cb2333f1b6615b46849ac18
verveaiofficial <aistudio.verve@atomicmail.io>
2026-08-06 01:06:37 +0600

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ class MyClient(discord.Client):
             
             try:
                 response = client.models.generate_content(
-                    model='gemini-1.5-flash',
+                    model='gemini-2.5-flash',
                     contents=user_message,
                     config=types.GenerateContentConfig(
                         system_instruction=bot_system_prompt,
